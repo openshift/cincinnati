dd4f1dd16a163fdb45ae08e60cf097540f2b2e19
Stefan Junker <mail@stefanjunker.de>
2018-11-06 18:32:20 +0100

    tests: cover fetch_releases
    There is a race condition in the code which causes the release from the
    smallest layer to be used, as that layer will win the download race,
    instead of the first release found starting from the topmost layer.
    
    By enlarging the first layer significantly the race condition is
    reproducible.

--- graph-builder/src/release.rs
+++ graph-builder/src/release.rs
@@ -17,9 +17,9 @@ use semver::Version;
 use std::collections::HashMap;
 use std::fmt;
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct Metadata {
-    kind: MetadataKind,
+    pub kind: MetadataKind,
     pub version: Version,
 
     #[serde(default)]
@@ -43,7 +43,7 @@ impl fmt::Display for Metadata {
     }
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone, PartialEq)]
 pub enum MetadataKind {
     #[serde(rename = "cincinnati-metadata-v0")]
     V0,
