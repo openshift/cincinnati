12f36d703364a5e8832d9b275d29fe1565a0ef9d
Luca Bruno <luca.bruno@coreos.com>
2018-11-20 08:58:49 +0000

    policy-engine: gather and expose metrics
    This instruments policy-engine to gather some basic metrics (related
    to graph serving to clients) and to expose them in Prometheus textual
    format.

--- policy-engine/src/config.rs
+++ policy-engine/src/config.rs
@@ -1,16 +1,4 @@
-// Copyright 2018 Alex Crawford
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//! Command-line options for policy-engine.
 
 use hyper::Uri;
 use std::net::IpAddr;
@@ -32,4 +20,12 @@ pub struct Options {
     /// Port to which the server will bind
     #[structopt(long = "port", default_value = "8081")]
     pub port: u16,
+
+    /// Address on which the server will serve metrics.
+    #[structopt(long = "metrics_address", default_value = "127.0.0.1")]
+    pub metrics_address: IpAddr,
+
+    /// Port to which the metrics server will bind.
+    #[structopt(long = "metrics_port", default_value = "9081")]
+    pub metrics_port: u16,
 }
