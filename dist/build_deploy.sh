e7138e51a72854a4047354c84570cd212140ddfe
Stefan Junker <mail@stefanjunker.de>
2018-12-04 16:59:01 +0100

    dist: run build as user and add cleanup

--- dist/build_deploy.sh
+++ dist/build_deploy.sh
@@ -3,26 +3,27 @@
 set -e
 
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-IMAGE_BUILD="${IMAGE_BUILD:-clux/muslrust:1.30.0-stable}"
+source "${ABSOLUTE_PATH}/commons.sh"
+
 IMAGE="quay.io/app-sre/cincinnati"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-PROJECT_PARENT_DIR=$ABSOLUTE_PATH/../
 DOCKERFILE_DEPLOY="$ABSOLUTE_PATH/Dockerfile"
 RELEASE_DIR="${PROJECT_PARENT_DIR}/target/x86_64-unknown-linux-musl/release"
 RELEASE_OUTPUT_DIR="${PROJECT_PARENT_DIR}/release-$(date +'%Y%m%d.%H%M%S')"
 
 function cleanup() {
+    set +e
     if [[ ! -n "$KEEP_RELEASE_OUTPUT" ]]; then
         rm -f ${RELEASE_OUTPUT_DIR}/{graph-builder,policy-engine}
         rmdir ${RELEASE_OUTPUT_DIR}
     fi
+    docker_cargo clean
 }
+trap cleanup EXIT
 
-docker run -t --rm -v $PROJECT_PARENT_DIR:/volume:Z $IMAGE_BUILD cargo build --release
-
+docker_cargo build --release
 mkdir $RELEASE_OUTPUT_DIR
 cp ${RELEASE_DIR}/{graph-builder,policy-engine} $RELEASE_OUTPUT_DIR/
-trap cleanup EXIT
 
 docker build -f $DOCKERFILE_DEPLOY -t "${IMAGE}:${IMAGE_TAG}" $RELEASE_OUTPUT_DIR
 
