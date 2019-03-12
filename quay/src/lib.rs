e7b7f7ffd5eac93091b094e58848fbecde81386d
Luca Bruno <luca.bruno@coreos.com>
2019-03-12 14:16:43 +0000

    quay: tweak read credentials logic
    This updates the logic for reading credentials, getting rid of Option
    handling and using generic path references instead.

--- quay/src/lib.rs
+++ quay/src/lib.rs
@@ -13,29 +13,27 @@ use failure::Fallible;
 use failure::ResultExt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
+use std::path::Path;
 
 pub mod v1;
 
-pub fn read_credentials(credentials_path: Option<&PathBuf>) -> Fallible<Option<String>> {
-    match &credentials_path {
-        Some(pathbuf) => {
-            let file =
-                File::open(pathbuf).context(format!("could not open '{}'", &pathbuf.display()))?;
+pub fn read_credentials<P>(path: P) -> Fallible<String>
+where
+    P: AsRef<Path>,
+{
+    let filepath = path.as_ref();
+    let file = File::open(filepath).context(format!("could not open '{}'", filepath.display()))?;
 
-            let first_line = BufReader::new(file)
-                .lines()
-                .nth(0)
-                .ok_or_else(|| format_err!("empty credentials."))?;
+    let first_line = BufReader::new(file)
+        .lines()
+        .nth(0)
+        .ok_or_else(|| format_err!("empty credentials."))?;
 
-            let token = first_line?.trim_end().to_string();
+    let token = first_line?.trim_end().to_string();
 
-            if token.is_empty() {
-                bail!("found an empty first line in '{}'", &pathbuf.display())
-            }
-
-            Ok(Some(token))
-        }
-        None => Ok(None),
+    if token.is_empty() {
+        bail!("found an empty first line in '{}'", filepath.display())
     }
+
+    Ok(token)
 }
