4a1c889894336fa1e1fe2f4271bf1ef9c99e73f3
Stefan Junker <mail@stefanjunker.de>
2019-04-03 12:23:08 +0200

    cincinnati/plugins: add common macro module
    This module implements macros which are helpful for all plugins.

---
+++ cincinnati/src/plugins/macros.rs
@@ -0,0 +1,81 @@
+//! Macro definitions to be used by all plugins
+
+#[macro_export]
+macro_rules! get_multiple_values {
+    ($map:expr, $key:expr ) => {
+        {
+            let closure = || {
+                Ok(
+                    if let Some(value) = $map.get($key) {
+                        value
+                    } else {
+                        bail!("could not find key '{}'", $key)
+                    },
+                )
+            };
+            closure()
+        }
+    };
+    ($map:expr, $( $key:expr ),* ) => {
+        {
+            let closure = || {
+                Ok(
+                    (
+                        $(
+                            if let Some(value) = $map.get($key) {
+                                value
+                            } else {
+                                bail!("could not find key '{}'", $key)
+                            },
+                        )*
+                    )
+                )
+            };
+            closure()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    #[test]
+    fn ensure_get_multiple_values() {
+        let params = [
+            ("a".to_string(), "a".to_string()),
+            ("b".to_string(), "b".to_string()),
+        ]
+        .iter()
+        .cloned()
+        .collect::<HashMap<String, String>>();
+
+        let (a, b): (&String, &String) = get_multiple_values!(params, "a", "b").unwrap();
+        assert_eq!((&"a".to_string(), &"b".to_string()), (a, b));
+
+        let params = [
+            ("a".to_string(), "a".to_string()),
+            ("b".to_string(), "b".to_string()),
+        ]
+        .iter()
+        .cloned()
+        .collect::<HashMap<String, String>>();
+
+        assert!(get_multiple_values!(params, "c").is_err());
+    }
+
+    #[test]
+    fn get_multiple_values_single() {
+        let params = [
+            ("a".to_string(), "a".to_string()),
+            ("b".to_string(), "b".to_string()),
+        ]
+        .iter()
+        .cloned()
+        .collect::<HashMap<String, String>>();
+
+        let a: &String = get_multiple_values!(params, "a").unwrap();
+        assert_eq!(&"a".to_string(), a);
+    }
+
+}
