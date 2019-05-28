7eb810d8ec326cf0fe7fe18dd74c87b11c10ed2a
Stefan Junker <mail@stefanjunker.de>
2019-05-28 21:31:48 +0200

    *: complete and move metadata fetch tests to the plugin

--- cincinnati/src/plugins/internal/metadata_fetch_quay.rs
+++ cincinnati/src/plugins/internal/metadata_fetch_quay.rs
@@ -178,3 +178,185 @@ impl InternalPlugin for QuayMetadataFetchPlugin {
         Ok(InternalIO { graph, parameters })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    fn init_logger() {
+        let _ = env_logger::try_init_from_env(env_logger::Env::default());
+    }
+
+    fn expected_metadata_labels_test_annoated(
+        manifestrefs: HashMap<usize, &str>,
+    ) -> HashMap<usize, HashMap<String, String>> {
+        [
+            (0, HashMap::new()),
+            (
+                1,
+                [
+                    (
+                        String::from(DEFAULT_QUAY_MANIFESTREF_KEY),
+                        manifestrefs
+                            .get(&1)
+                            .expect("expected manifestref")
+                            .to_string(),
+                    ),
+                    (String::from("kind"), String::from("test")),
+                    (
+                        String::from("io.openshift.upgrades.graph.previous.remove"),
+                        String::from("0.0.0"),
+                    ),
+                ]
+                .iter()
+                .cloned()
+                .collect(),
+            ),
+            (
+                2,
+                [
+                    (
+                        String::from(DEFAULT_QUAY_MANIFESTREF_KEY),
+                        manifestrefs
+                            .get(&2)
+                            .expect("expected manifestref")
+                            .to_string(),
+                    ),
+                    (
+                        String::from("io.openshift.upgrades.graph.release.remove"),
+                        String::from("true"),
+                    ),
+                ]
+                .iter()
+                .cloned()
+                .collect(),
+            ),
+            (
+                3,
+                [
+                    (
+                        String::from(DEFAULT_QUAY_MANIFESTREF_KEY),
+                        manifestrefs
+                            .get(&3)
+                            .expect("expected manifestref")
+                            .to_string(),
+                    ),
+                    (
+                        String::from("io.openshift.upgrades.graph.previous.add"),
+                        String::from("0.0.1,0.0.0"),
+                    ),
+                ]
+                .iter()
+                .cloned()
+                .collect(),
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect()
+    }
+
+    #[cfg(feature = "test-net")]
+    #[test]
+    fn metadata_fetch_from_public_quay_succeeds() {
+        init_logger();
+
+        let manifestrefs = [
+            (0, ""),
+            (
+                1,
+                "sha256:0275e5e316373faaabea9f13dfc27541e3c6e301b08bd92f443e987195faa9d6",
+            ),
+            (
+                2,
+                "sha256:e6077b9aee2bb5dae2d90d91ce2165cee802d84ce1af45e281cba47950a37f39",
+            ),
+            (
+                3,
+                "sha256:9ad8330c3b697d0631083edf72634ddf2ad1d50982d7090faf36c4a1f7eae10f",
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let metadata = expected_metadata_labels_test_annoated(manifestrefs);
+
+        let input_graph: crate::Graph =
+            crate::tests::generate_custom_graph(0, metadata.len(), metadata.clone(), None);
+
+        let expected_graph: crate::Graph =
+            crate::tests::generate_custom_graph(0, metadata.len(), metadata, None);
+
+        let processed_graph = QuayMetadataFetchPlugin::try_new(
+            "redhat/openshift-cincinnati-test-labels-public-manual".to_string(),
+            DEFAULT_QUAY_LABEL_FILTER.to_string(),
+            DEFAULT_QUAY_MANIFESTREF_KEY.to_string(),
+            None,
+            quay::v1::DEFAULT_API_BASE.to_string(),
+        )
+        .expect("could not initialize the QuayMetadataPlugin")
+        .run_internal(InternalIO {
+            graph: input_graph,
+            parameters: Default::default(),
+        })
+        .expect("plugin run failed")
+        .graph;
+
+        assert_eq!(expected_graph, processed_graph);
+    }
+
+    #[cfg(feature = "test-net-private")]
+    #[test]
+    fn metadata_fetch_from_private_quay_succeeds() {
+        init_logger();
+
+        let token_file = std::env::var("CINCINNATI_TEST_QUAY_API_TOKEN_PATH")
+            .expect("CINCINNATI_TEST_QUAY_API_TOKEN_PATH missing");
+
+        let manifestrefs = [
+            (0, ""),
+            (
+                1,
+                "sha256:278fd5a7193c183f2b71523fa605b3319bfa58e4230a725a6518f1b4bd5f1437",
+            ),
+            (
+                2,
+                "sha256:9867bd09390bcfed9d21b61083def233bc9451984b11de725597939240146424",
+            ),
+            (
+                3,
+                "sha256:0f4446af92a57308180017404db3d8cf0ca20101c0d83ae76e1fc14def338399",
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let metadata = expected_metadata_labels_test_annoated(manifestrefs);
+
+        let input_graph: crate::Graph =
+            crate::tests::generate_custom_graph(0, metadata.len(), metadata.clone(), None);
+
+        let expected_graph: crate::Graph =
+            crate::tests::generate_custom_graph(0, metadata.len(), metadata, None);
+
+        let processed_graph = QuayMetadataFetchPlugin::try_new(
+            "redhat/openshift-cincinnati-test-labels-private-manual".to_string(),
+            DEFAULT_QUAY_LABEL_FILTER.to_string(),
+            DEFAULT_QUAY_MANIFESTREF_KEY.to_string(),
+            Some(token_file.into()),
+            quay::v1::DEFAULT_API_BASE.to_string(),
+        )
+        .expect("could not initialize the QuayMetadataPlugin")
+        .run_internal(InternalIO {
+            graph: input_graph,
+            parameters: Default::default(),
+        })
+        .expect("plugin run failed")
+        .graph;
+
+        assert_eq!(expected_graph, processed_graph);
+    }
+}
