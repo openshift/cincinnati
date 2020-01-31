49fce172e17de895efa9d334cd4309475b093000
Stefan Junker <mail@stefanjunker.de>
2020-01-31 23:05:32 +0100

    graph-builder/tests/e2e: adapt to request bump

--- graph-builder/tests/e2e/mod.rs
+++ graph-builder/tests/e2e/mod.rs
@@ -10,6 +10,8 @@ use url::Url;
 #[test_case("b",    "amd64", include_str!("./testdata/b-amd64.json");    "channel b amd64")]
 #[test_case("test", "amd64", include_str!("./testdata/test-amd64.json"); "channel test amd64")]
 fn e2e_channel_success(channel: &'static str, arch: &'static str, testdata: &str) {
+    let mut runtime = commons::testing::init_runtime().unwrap();
+
     let graph_base_url = match env::var("GRAPH_URL") {
         Ok(env) => env,
         _ => panic!("GRAPH_URL unset"),
@@ -25,15 +27,19 @@ fn e2e_channel_success(channel: &'static str, arch: &'static str, testdata: &str
 
     println!("Querying {}", graph_url.to_string());
 
-    let mut res = reqwest::ClientBuilder::new()
-        .danger_accept_invalid_certs(true)
-        .build()
-        .unwrap()
-        .get(&graph_url.to_string())
-        .header(ACCEPT, HeaderValue::from_static("application/json"))
-        .send()
+    let res = runtime
+        .block_on(
+            reqwest::ClientBuilder::new()
+                .danger_accept_invalid_certs(true)
+                .build()
+                .unwrap()
+                .get(&graph_url.to_string())
+                .header(ACCEPT, HeaderValue::from_static("application/json"))
+                .send(),
+        )
         .unwrap();
     assert_eq!(res.status().is_success(), true);
-    let actual = serde_json::from_str(&res.text().unwrap()).unwrap();
+    let text = runtime.block_on(res.text()).unwrap();
+    let actual = serde_json::from_str(&text).unwrap();
     assert_json_include!(actual: actual, expected: expected)
 }
