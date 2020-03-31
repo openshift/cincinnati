235f6a6e0d1ae1622b3483ce31333b8bbe0e23b8
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-03-31 23:54:11 +0200

    hack/load-testing: set max-workers

--- hack/load-testing.sh
+++ hack/load-testing.sh
@@ -17,7 +17,7 @@ for workers in 10 50 100; do
     file="${TMP_DIR}/rate-${rate}-workers-${workers}.bin"
     echo "Testing workers ${workers}, rate ${rate} -> ${file}"
     sed "s,GRAPH_URL,${GRAPH_URL},g" vegeta.targets | \
-      vegeta attack -format http -workers=${workers} -rate=${rate} -duration ${duration} > ${file}
+      vegeta attack -format http -workers=${workers} -max-workers=${workers} -rate=${rate} -duration ${duration} > ${file}
     vegeta report -type=text ${file}
     # Sleep here to clear up connections cache in cincinnati
     sleep 30
