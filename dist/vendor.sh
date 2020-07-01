8f63b910774a3e18f79c3c73bf2082479d0f4edb
Stefan Junker <mail@stefanjunker.de>
2020-07-01 18:43:01 +0200

    dist/vendor: handle rh-manifest.txt

--- dist/vendor.sh
+++ dist/vendor.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
 CARGO_CONFIG=".cargo/config.toml"
+FILES_TO_COMMIT=(
+  ".cargo"
+  "vendor"
+  "rh-manifest.txt"
+)
 
 # Don't overwrite any existing config
 [ ! -f "${CARGO_CONFIG}" ] || {
@@ -10,11 +15,15 @@ CARGO_CONFIG=".cargo/config.toml"
 
 set -xueo pipefail
 
+# generate the rh-manifest.txt
+cargo run --bin rh-manifest-generator
+
 mkdir -p "$(dirname ${CARGO_CONFIG})"
 cargo vendor > "${CARGO_CONFIG}"
 
 # some vendored files aren't world readable which has lead to issues in container builds when trying out the workflow
 find ./vendor -type f -exec chmod a+r {} \;
 
-git add --force .cargo vendor
-git commit -m "Vendor dependencies and update cargo to use them" -- .cargo vendor
+
+git add --force "${FILES_TO_COMMIT[@]}"
+git commit -m "Vendor dependencies and update cargo to use them, and add rh-manifest.txt" -- "${FILES_TO_COMMIT[@]}"
