37ac6452ba4effedc6bbb3f58229e659a2f84347
Stefan Junker <mail@stefanjunker.de>
2020-10-02 21:56:26 +0200

    dist/tests: add scripts to manage CI images and credentials
    * Add script to prepare the credentials on CI
    * Add script to push images to the CI registry
    * Support pushing test images with authentication

---
+++ dist/prepare_e2e_images.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+skopeo sync --src docker --dest docker --src-no-creds --dest-authfile "${CINCINNATI_CI_PUBLIC_DOCKERJSON_PATH}" \
+  quay.io/openshift-release-dev/ocp-release \
+  registry.svc.ci.openshift.org/cincinnati-ci-public
