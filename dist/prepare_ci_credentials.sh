4b0867ba74daceb2677890fc6309e16c2e3286b0
Stefan Junker <mail@stefanjunker.de>
2020-10-14 15:27:23 +0200

    e2e: install kubic skopeo version and copy credential and e2e_image script

--- dist/prepare_ci_credentials.sh
+++ dist/prepare_ci_credentials.sh
@@ -4,12 +4,14 @@
 
 set -xeuo pipefail
 
-test -f "${CINCINNATI_CI_DOCKERCFG_PATH}"
-test -f "${CINCINNATI_CI_PUBLIC_DOCKERCFG_PATH}"
+CINCINNATI_CI_DOCKERCFG_PATH="${CINCINNATI_CI_DOCKERCFG_PATH:-/dev/null}"
+CINCINNATI_CI_PUBLIC_DOCKERCFG_PATH="${CINCINNATI_CI_PUBLIC_DOCKERCFG_PATH:-/dev/null}"
 
-touch "${CINCINNATI_CI_DOCKERJSON_PATH}"
-touch "${CINCINNATI_CI_PUBLIC_DOCKERJSON_PATH}"
+if test -f "${CINCINNATI_CI_DOCKERCFG_PATH}"; then
+    # consolidate the quay credentials with the private CI registry credentials to allow a seamless transition to the latter
+    jq -s '{ "auths": .[0] } * .[1]' "${CINCINNATI_CI_DOCKERCFG_PATH}" "${CINCINNATI_TEST_CREDENTIALS_PATH}" > "${CINCINNATI_CI_DOCKERJSON_PATH}"
+fi
 
-# consolidate the quay credentials with the private CI registry credentials to allow a seamless transition to the latter
-jq -s '{ "auths": .[0] } * .[1]' "${CINCINNATI_CI_DOCKERCFG_PATH}" "${CINCINNATI_TEST_CREDENTIALS_PATH}" > "${CINCINNATI_CI_DOCKERJSON_PATH}"
-jq <"${CINCINNATI_CI_PUBLIC_DOCKERCFG_PATH}" '{ "auths": . }' > "${CINCINNATI_CI_PUBLIC_DOCKERJSON_PATH}"
+if test -f "${CINCINNATI_CI_PUBLIC_DOCKERCFG_PATH}"; then
+    jq <"${CINCINNATI_CI_PUBLIC_DOCKERCFG_PATH}" '{ "auths": . }' > "${CINCINNATI_CI_PUBLIC_DOCKERJSON_PATH}"
+fi
