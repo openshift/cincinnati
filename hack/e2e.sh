37147dbcd08e9034c93587c9d568f2f4824b4026
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-10-21 22:01:48 +0200

    hack/e2e.sh: set fetch_concurrency to 16

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -100,7 +100,7 @@ backoff oc new-app -f dist/openshift/cincinnati.yaml \
       name = "release-scrape-dockerv2"
       registry = "${E2E_SCRAPE_REGISTRY:-quay.io}"
       repository = "${E2E_SCRAPE_REPOSITORY:-openshift-release-dev/ocp-release}"
-      fetch_concurrency = 128
+      fetch_concurrency = 16
 
       [[plugin_settings]]
       name = "github-secondary-metadata-scrape"
