9352c407aeb3a64337b3eafd941bc35c0d820a7d
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-11-02 13:32:02 +0100

    Use OSUS operator to setup test Cincinnati
    Instead of shipping our template and custom config, we should use OSUS operator to run Cincinnati

--- e2e/tests/slo.rs
+++ e2e/tests/slo.rs
@@ -46,8 +46,8 @@ fn check_slo_exact(query: &'static str) -> String {
 }
 
 // Service reports at all times
-#[test_case(r#"min_over_time(up{job="cincinnati-policy-engine"}[1h])"# => 1)]
-#[test_case(r#"min_over_time(up{job="cincinnati-graph-builder"}[1h])"# => 1)]
+#[test_case(r#"min_over_time(up{job="e2e-policy-engine"}[1h])"# => 1)]
+#[test_case(r#"min_over_time(up{job="e2e-graph-builder"}[1h])"# => 1)]
 // No scrape errors
 #[test_case("cincinnati_gb_graph_upstream_errors_total" => is less_than(1))]
 // No upstream errors
