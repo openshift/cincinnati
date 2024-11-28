6ab667a8e6f698c0bbb44e6664e54926e3cedf88
David Hurta <dhurta@redhat.com>
2024-11-28 16:49:31 +0100

    github_scraper tests: Ensure tar file exists
    Insert the secondary metadata tar file as a parameter even when no new
    commits are observed.

--- cincinnati/src/plugins/internal/graph_builder/github_openshift_secondary_metadata_scraper/plugin.rs
+++ cincinnati/src/plugins/internal/graph_builder/github_openshift_secondary_metadata_scraper/plugin.rs
@@ -484,6 +484,15 @@ impl InternalPlugin for GithubOpenshiftSecondaryMetadataScraperPlugin {
                 .to_string(),
         );
 
+        let graph_data_tar_path = self.settings.output_directory.join("graph-data.tar.gz");
+        io.parameters.insert(
+            SECONDARY_METADATA_PARAM_KEY.to_string(),
+            graph_data_tar_path
+                .to_str()
+                .ok_or_else(|| format_err!("secondary_metadata path cannot be converted to str"))?
+                .to_string(),
+        );
+
         let should_update = self
             .refresh_commit_wanted()
             .await
@@ -499,7 +508,6 @@ impl InternalPlugin for GithubOpenshiftSecondaryMetadataScraperPlugin {
                 .await
                 .context("Extracting tarball")?;
 
-            let graph_data_tar_path = self.settings.output_directory.join("graph-data.tar.gz");
             let signatures_path = graph_data_dir.as_path().join("signatures");
             let signatures_symlink = self.settings.output_directory.join("signatures");
 
@@ -521,16 +529,6 @@ impl InternalPlugin for GithubOpenshiftSecondaryMetadataScraperPlugin {
             )
             .await
             .context("creating graph-data tar")?;
-
-            io.parameters.insert(
-                SECONDARY_METADATA_PARAM_KEY.to_string(),
-                graph_data_tar_path
-                    .to_str()
-                    .ok_or_else(|| {
-                        format_err!("secondary_metadata path cannot be converted to str")
-                    })?
-                    .to_string(),
-            );
         };
 
         Ok(io)
@@ -548,6 +546,12 @@ mod network_tests {
 
         let tmpdir = tempfile::tempdir()?;
 
+        // Path to the GitHub API token file; we need to use it in tests
+        // because GitHub rate limits unauthenticated API access.
+        // In CI this value comes from cincy-credentials secret in Vault, see:
+        // https://github.com/openshift/release/blob/cca454f13b1d8482a3a568bf557b37eeed4c7519/ci-operator/config/openshift/cincinnati/openshift-cincinnati-master.yaml#L109-L110
+        //
+        // Note: The token in CI may expire.
         let oauth_token_path = std::env::var(GITHUB_SCRAPER_TOKEN_PATH_ENV)?;
 
         let settings = GithubOpenshiftSecondaryMetadataScraperSettings::deserialize_config(
@@ -589,6 +593,16 @@ mod network_tests {
                 }
             };
 
+            let tar_path = io
+                .parameters
+                .get(SECONDARY_METADATA_PARAM_KEY)
+                .map(PathBuf::from)
+                .unwrap();
+
+            assert!(
+                tar_path.exists(),
+                "ensure the plugin creates a tar file in a specified directory"
+            );
 
             let regexes = DEFAULT_OUTPUT_ALLOWLIST
                 .iter()
