d2eff6c0792a2268530a2a26617efb7e29b2d1f6
Hongkai Liu <hongkailiu@users.noreply.github.com>
2025-05-05 11:24:02 -0400

    Clean up dkrv2_openshift_secondary_metadata_scraper/test_fixtures/build.sh

--- cincinnati/src/plugins/internal/graph_builder/dkrv2_openshift_secondary_metadata_scraper/test_fixtures/build.sh
+++ cincinnati/src/plugins/internal/graph_builder/dkrv2_openshift_secondary_metadata_scraper/test_fixtures/build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-set -xu
-export REPO_BASE="registry.ci.openshift.org/cincinnati-ci-public/cincinnati"
-export AUTHFILE="${CINCINNATI_CI_PUBLIC_DOCKERJSON_PATH}"
-../../../../../../../graph-builder/tests/images/build-n-push-buildah.sh graph-data-6420f7fbf3724e1e5e329ae8d1e2985973f60c14
+
+>&2 echo "The script is deprecated and the image registry.ci.openshift.org/cincinnati-ci-public/cincinnati-graph-data:6420f7fbf3724e1e5e329ae8d1e2985973f60c14 is built by https://github.com/openshift/release/tree/master/clusters/app.ci/supplemental-ci-images/cincinnati-ci/cincinnati-graph-data_is.yaml"
